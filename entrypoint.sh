c55d224779dec7bacadaf47f0659c13c8aeee022
bebben88 <32220954+bebben88@users.noreply.github.com>
2018-06-09 08:06:37 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -127,46 +127,6 @@ case ${OPENHAB_VERSION} in
     ;;
 esac
 
-# bluetooth
-# catch signals as PID 1 in a container
-
-pid=0
-
-# SIGNAL-handler
-term_handler() {
-
- echo "stopping bluetooth daemon ..."
- if [ $pid -ne 0 ]; then
-        kill -SIGTERM "$pid"
-        wait "$pid"
- fi
-
-  echo "bring hci0 down ..."
-  hciconfig hci0 down
-
-  echo "terminating dbus ..."
-  /etc/init.d/dbus stop
-
-  exit 143; # 128 + 15 -- SIGTERM
-}
-
-# on callback, stop all started processes in term_handler
-trap 'kill ${!}; term_handler' SIGINT SIGKILL SIGTERM SIGQUIT SIGTSTP SIGSTOP SIGHUP
-
-# start docker deamon
-echo "starting dbus ..."
-/etc/init.d/dbus start
-
-#start BCM chip
-hciattach /dev/ttyAMA0 bcm43xx 921600 noflow
-
-#create hci0 device
-hciconfig hci0 up
-
-#start bluetooth daemon
-/usr/libexec/bluetooth/bluetoothd -d &
-pid="$!"
-
 # Set openhab folder permission
 chown -R openhab:openhab ${APPDIR}
 
